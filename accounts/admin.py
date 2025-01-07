e129af33e2582ae280cbe411404fadd0da0c7b25
Maan <marmughanjaved@gmail.com>
2025-01-07 22:47:25 +0500

    fixed the add_fieldset erorr that appears while adding a new user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "age",
+        "is_staff",
+        "is_active",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = (
+        None, 
+        {
+        "classes": ("wide",),
+        "fields": ("username", "password1", "password2", "age", "is_staff", "is_active"),
+        }
+    ),
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
