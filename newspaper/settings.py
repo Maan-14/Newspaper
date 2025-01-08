d34f0f33d5a8d5816a0fda9d9b35d1b8027945ab
Maan <marmughanjaved@gmail.com>
2025-01-08 18:40:51 +0500

    password change functionality

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +132,8 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = "home" # new
 LOGOUT_REDIRECT_URL = "home" # new
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" # new
+CRISPY_TEMPLATE_PACK = "bootstrap5" # new
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" # new
